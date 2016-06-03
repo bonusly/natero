9c0c0a06052b1570b7f15e6c0c8c44171cc43c90
markovAntony <abbrown22@gmail.com>
2016-06-03 09:55:54 -0600

    add configuration

--- natero/lib/natero.rb
+++ natero/lib/natero.rb
@@ -1,5 +1,21 @@
 require "natero/version"
 
 module Natero
-  # Your code goes here...
+  class Configuration
+    attr_accessor :account_api_key, :event_api_key, :event_auth_key
+
+    def initialize
+      self.account_api_key = nil
+      self.event_api_key = nil
+      self.event_auth_key = nil
+    end
+
+    def self.configuration
+      @configuration ||= Configuration.new
+    end
+
+    def self.configure
+      yield(configuration) if block_given?
+    end
+  end
 end
